cb6c08ea0f65857fda62633a59ed679d5aa27c7f
Andrey <LollipopG2357@gmail.com>
2023-07-30 08:49:41 +0300

    Add test (summ)

--- LearnC#/Program.cs
+++ LearnC#/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 10;
+            int b = 20;
+            int sum = a + b;
+            Console.WriteLine($"Sum = {sum}");
         }
     }
 }
