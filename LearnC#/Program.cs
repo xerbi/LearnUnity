1ffd70b066cc26a7e74d5d3c120bf38861b00e82
Andrey <LollipopG2357@gmail.com>
2023-08-06 08:44:21 +0300

    Add Tasks

--- LearnC#/Program.cs
+++ LearnC#/Program.cs
@@ -1,17 +1,94 @@
-﻿namespace LearnC_
+﻿using System.Drawing;
+
+namespace LearnC_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int a = 10;
-            int b = 20;
-            Console.WriteLine($"Sum = {summ(a, b)}");
+            LogicTask1();
         }
 
-        static int summ(int a, int b)
+        // Вывод массива в консоль!
+        static void ShowArray(Array mas)
         {
-            return a + b;
+            foreach (var a in mas)
+            {
+                Console.Write($"{a} ");
+            }
+            Console.WriteLine();
+        }
+
+        /*
+         * Создайте массив, в котором 
+         * количество отрицательных чисел 
+         * равно количеству положительных 
+         * и положительные числа расположены 
+         * на случайных местах в массиве. (normal)
+         */
+        static void RandomMassive_v1(int size = 10) // size - необязательный параметр
+        {
+            int[] arr = new int[size];
+            Random rand = new Random();
+
+            int posituve = 0, negative = 0;
+            int middle = size / 2;
+
+            for (int i = 0; i < size; i++)
+            {
+                if (posituve < middle && negative < middle)
+                {
+                    arr[i] = rand.Next(-100, 100);
+                    if (arr[i] > 0) posituve++;
+                    else negative++;
+                    continue;
+                }
+
+                if (posituve != middle)
+                {
+                    arr[i] = rand.Next(100);
+                    continue;
+                }
+
+                if (negative != middle)
+                {
+                    arr[i] = rand.Next(100)*(-1);
+                }
+            }
+
+            ShowArray(arr);
+        }//static void RandomMassive_v1(int size = 10)
+
+        /*
+         * Дано трехзначное число.
+         *   а) Верно ли, что все его цифры одинаковые?
+         *   б) Определить, есть ли среди его цифр одинаковы
+         */
+
+        static void LogicTask1(int number = 919)
+        {
+            string str = number.ToString();
+
+            int i = 0;
+            int size = str.Length;
+            for (i = 1; i < size; i++)
+            {
+                if (str[i] != str[i - 1]) break;
+            }
+
+            //а) Верно ли, что все его цифры одинаковые?
+            if (i != size) Console.WriteLine("Верно ли, что все его цифры одинаковые? НЕТ");
+            else Console.WriteLine("Верно ли, что все его цифры одинаковые? ДА");
+
+            //б) Определить, есть ли среди его цифр одинаковы
+            int counter = 0;
+            for (i = 0; i < size; i++)
+            {
+               for (int j = 1; j < size; ++j)
+                {
+                    if (str[i] == str[j]) break; 
+                }
+            }
         }
     }
 }
